206a6ab2022ea90efc94b10f3c6bf831003a1567
Your Name <you@example.com>
2021-06-15 17:41:52 -0300

    archivo para carga de numeros primos

---
+++ carga_de_primos.py
@@ -0,0 +1,30 @@
+import os
+import re
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siselecc.local")
+django.setup()
+
+# from apps.gest_cifrado.models import SecuenciaPrimo
+
+
+def get_rutas():
+    # ruta_base = os.path.dirname(__file__)
+    ruta_base = os.getcwd()
+    return [ruta_base+'\\300txt\\'+f'{(x*240100)}.txt' for x in range(300)]
+
+
+"""
+def escribir_objeto(indice, ruta):
+    print(indice)
+    f = open(ruta, 'r')
+    SecuenciaPrimo.objects.create(indice=indice,
+                                  secuencia={
+                                      'indice': indice,
+                                      'secuencia': re.findall('\d+', f.read())})
+    f.close()
+"""
+
+if __name__ == "__main__":
+    rutas = get_rutas()
+    # [escribir_objeto((x*240100),rutas[x]) for x in range(300)]
