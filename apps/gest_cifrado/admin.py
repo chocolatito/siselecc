077ed07257efc331afbdf23087abebe5fc4a66fc
Your Name <you@example.com>
2021-06-15 10:46:38 -0300

    administrador para secuencia de primos

--- apps/gest_cifrado/admin.py
+++ apps/gest_cifrado/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Clave
+from import_export.admin import ImportExportModelAdmin
+from .models import Clave, SecuenciaPrimo
 # Register your models here.
+
+
+class SecuenciaPrimoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['indice']
+
+
+admin.site.register(SecuenciaPrimo, SecuenciaPrimoAdmin)
 
 admin.site.register(Clave)
