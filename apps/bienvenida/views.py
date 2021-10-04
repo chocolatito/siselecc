2f6d4f7685de9f32dd18975362f5dea9c69f9623
chocolatito <usuariodetelegram@gmail.com>
2021-10-04 16:53:50 -0300

    <p class="lead">. . .</p>

--- apps/bienvenida/views.py
+++ apps/bienvenida/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
+from ..gest_preparacion.models import Eleccion
 # from ..gest_cifrado.utils import generar_secuencias  # eliminar
 
 # Create your views here.
@@ -11,8 +12,14 @@ from django.views.generic import TemplateView
 class Bienvenida(TemplateView):
     template_name = "bienvenida/bienvenida.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Bienvenido'
-        context['iterador'] = [x for x in range(20)]
+        # Elecciones programadas, en curso y cerradas
+        context['proximas'] = Eleccion.objects.filter(etapa__in=[1, 2])
+        context['en_curso'] = Eleccion.objects.filter(etapa=3)
+        context['cerradas'] = Eleccion.objects.filter(etapa_in=[4, 5, 6])
         return context
