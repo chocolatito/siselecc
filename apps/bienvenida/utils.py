172d5c258f35bfbea49eb62e8ff465e41df8a345
chocolatito <usuariodetelegram@gmail.com>
2021-10-04 21:41:40 -0300

    Update utils.py

--- apps/bienvenida/utils.py
+++ apps/bienvenida/utils.py
@@ -6,3 +6,4 @@ def get_escrutinio(boletas, v_resultado):
     escrutinio.append(('En Blanco', v_resultado[0],))
     # sort by second element of tuple
     escrutinio.sort(reverse=True, key=lambda x: x[1])  # index 1 means second element
+    return escrutinio
