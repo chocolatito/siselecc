d7fd88eaaea25a215c70d24f67637bea70d0ed2f
chocolatito <usuariodetelegram@gmail.com>
2021-10-05 12:11:51 -0300

    Update utils.py

--- apps/bienvenida/utils.py
+++ apps/bienvenida/utils.py
@@ -14,7 +14,7 @@ def get_escrutinio(boletas, v_res, total_votos):
     escrutinio = [(b.candidato, v_res[b.indice], get_porcentaje(
         v_res[b.indice])//total_votos) for b in boletas]
     # ORDENAR
-    escrutinio.append(('En Blanco', v_res[0], get_porcentaje(v_res[0]//total_votos))
+    escrutinio.append(('En Blanco', v_res[0], get_porcentaje(v_res[0]//total_votos)))
     # sort by second element of tuple
     escrutinio.sort(reverse=True, key=lambda x: x[1])  # index 1 means second element
     return escrutinio
